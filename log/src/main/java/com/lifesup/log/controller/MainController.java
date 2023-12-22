65ebff13e94189b412844c659f5035ef19972833
TTS1 <tts1@lifesup.local>
2023-12-22 17:08:03 +0700

    third

--- log/src/main/java/com/lifesup/log/controller/MainController.java
+++ log/src/main/java/com/lifesup/log/controller/MainController.java
@@ -27,6 +27,63 @@ public class MainController {
             throw new RuntimeException("log not found for the id "+id);
         }
     }
+    @GetMapping("/logs/sort")
+    public List<LogEntity> sortByDate(@RequestParam String collum_name){
+        if (collum_name.equals("Created_At_Desc")){
+        return logRepo.findAllByOrderByCreatedAtDesc();
+        }
+        if (collum_name.equals("Updated_At_Desc")) {
+            return logRepo.findAllByOrderByUpdatedAtDesc();
+        }
+        if (collum_name.equals("Model")) {
+            return logRepo.findAllByOrderByModel();
+        }
+        if (collum_name.equals("Total_Cost_Asc")) {
+            return logRepo.findAllByOrderByTotalCostAsc();
+        }
+        if (collum_name .equals("Total_Cost_Desc")) {
+            return logRepo.findAllByOrderByTotalCostDesc();
+        }
+        if (collum_name .equals("Promt_Cost_Asc")) {
+            return logRepo.findAllByOrderByPromtCostAsc();
+        }
+        if (collum_name .equals("Promt_Cost_Desc")) {
+            return logRepo.findAllByOrderByPromtCostDesc();
+        }
+        if (collum_name .equals("Promt_Token_Asc")) {
+            return logRepo.findAllByOrderByPromtTokensAsc();
+        }
+        if (collum_name .equals("Promt_Token_Desc")) {
+            return logRepo.findAllByOrderByPromtTokensDesc();
+        }
+        if (collum_name .equals("Completion_Cost_Asc")) {
+            return logRepo.findAllByOrderByCompletionCostAsc();
+        }
+        if (collum_name .equals("Completion_Cost_Desc")) {
+            return logRepo.findAllByOrderByCompletionCostDesc();
+        }
+        if (collum_name .equals("Completion_Token_Asc")) {
+            return logRepo.findAllByOrderByCompletionTokensAsc();
+        }
+        if (collum_name .equals("Completion_Token_Desc")) {
+            return logRepo.findAllByOrderByCompletionTokensDesc();
+        }
+        if (collum_name .equals("Total_Token_Asc")) {
+            return logRepo.findAllByOrderByTotalTokensAsc();
+        }
+        if (collum_name .equals("Total_Token_Desc")) {
+            return logRepo.findAllByOrderByTotalTokensDesc();
+        }
+        return logRepo.findAll();
+    }
+    @GetMapping("/search")
+    public List<LogEntity> searchAllFields(@RequestParam(name = "find") String keyword) {
+        System.out.println(keyword);
+        if (keyword == null) {
+            return logRepo.findAll();
+        }
+        return logRepo.searchAllFields(keyword);
+    }
     @DeleteMapping("/log")
     public String delete(@RequestParam int id) {
         Optional<LogEntity> log = logRepo.findById(id);
--- log/src/main/java/com/lifesup/log/repository/LogRepository.java
+++ log/src/main/java/com/lifesup/log/repository/LogRepository.java
@@ -2,9 +2,47 @@ package com.lifesup.log.repository;
 
 import com.lifesup.log.model.LogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LogRepository extends JpaRepository<LogEntity, Integer> {
 
+    List<LogEntity> findAllByOrderByCreatedAtDesc();
+    List<LogEntity> findAllByOrderByUpdatedAtDesc();
+    List<LogEntity> findAllByOrderByModel();
+    @Query("SELECT l FROM LogEntity l ORDER BY l.total_cost DESC")
+    List<LogEntity> findAllByOrderByTotalCostDesc();
+    @Query("SELECT l FROM LogEntity l ORDER BY l.total_cost ASC")
+    List<LogEntity> findAllByOrderByTotalCostAsc();
+    @Query("SELECT l FROM LogEntity l ORDER BY l.promt_tokens DESC")
+    List<LogEntity> findAllByOrderByPromtTokensDesc();
+    @Query("SELECT l FROM LogEntity l ORDER BY l.promt_tokens ASC")
+    List<LogEntity> findAllByOrderByPromtTokensAsc();
+    @Query("SELECT l FROM LogEntity l ORDER BY l.promt_cost DESC")
+    List<LogEntity> findAllByOrderByPromtCostDesc();
+    @Query("SELECT l FROM LogEntity l ORDER BY l.promt_cost ASC")
+    List<LogEntity> findAllByOrderByPromtCostAsc();
+    @Query("SELECT l FROM LogEntity l ORDER BY l.completion_tokens DESC")
+    List<LogEntity> findAllByOrderByCompletionTokensDesc();
+    @Query("SELECT l FROM LogEntity l ORDER BY l.completion_tokens ASC")
+    List<LogEntity> findAllByOrderByCompletionTokensAsc();
+    @Query("SELECT l FROM LogEntity l ORDER BY l.completion_cost DESC")
+    List<LogEntity> findAllByOrderByCompletionCostDesc();
+    @Query("SELECT l FROM LogEntity l ORDER BY l.completion_cost ASC")
+    List<LogEntity> findAllByOrderByCompletionCostAsc();
+    @Query("SELECT l FROM LogEntity l ORDER BY l.total_tokens DESC")
+    List<LogEntity> findAllByOrderByTotalTokensDesc();
+    @Query("SELECT l FROM LogEntity l ORDER BY l.total_tokens ASC")
+    List<LogEntity> findAllByOrderByTotalTokensAsc();
+    //search all fields
+    @Query("SELECT l FROM LogEntity l WHERE " +
+            "LOWER(l.model) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(l.full_message) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(l.promt) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(l.user_message) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(l.reply) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<LogEntity> searchAllFields(String keyword);
 }
